46f14d02fd5d882aea486c2b795a76de1332a45c
Saikat-too <saikatsingha00q@gmail.com>
2024-12-07 23:33:15 +0600

    sign in and sign up setup

---
+++ server/route/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUsers,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUser,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/user.js";
+import { protect, admin } from "../middlewear/auth.js";
+
+router.route("/").post(registerUser).get(protect, admin, getUser);
+router.post("/login", authUsers);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./route/userRoutes.js";
 const app = express();
 app.use(express.json);
+
+app.use("/api/users", userRoutes);
 
 dotenv.config();
 connectDB();
